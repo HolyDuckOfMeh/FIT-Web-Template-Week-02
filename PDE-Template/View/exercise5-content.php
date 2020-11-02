770b8ebad8419d1401cdf9f430eab808a3e034d6
Jason Zheng <jzheng@training.local>
2020-11-02 16:41:31 -0500

    finished questions 1 and 2

--- PDE-Template/View/exercise5-content.php
+++ PDE-Template/View/exercise5-content.php
@@ -8,7 +8,24 @@
       (You may use "skeleton" as the keyword if you can't think of anything else.)</h4>
     <!-- Place Answer Here -->
 
+      <label for='leagueChatCleanser'>League Chat Input:</label>
+      <input id='leagueChatInput'>
+      <p id='leagueChatDisplay'></p>
+
+      <button id='leagueChatButton' onClick='chat(document.getElementById("leagueChatInput").value)'>Trash Talk</button>
+
+      <script>
+
+       function chat(input)
+          {
+            var ezclap=input.indexOf("ez");
+            var keyMessage=input.substring(ezclap, ezclap+2);
+            document.getElementById('leagueChatDisplay').innerHTML = keyMessage
+          }
       
+      </script>
+
+
 
     <!-- Place Answer Here -->
   </div>
@@ -23,7 +40,39 @@
         (Hint two arrays or a 2 dimensional array is needed)</h4>
     <!-- Place Answer Here -->
       
+      <label for='gameInventoryInput'>Item Name:</label>
+      <input id='gameInventoryInput'>
 
+      <button id='gameInventoryButton' onClick='inventorySearch(document.getElementById("gameInventoryInput").value)'>Search Bag</button>
+      <p id='gameDisplay'></p>
+      <br>
+      <br>
+      <br>
+
+      <label for='gameAmountInput'>Item Amount:</label>
+      <input id='gameAmountInput'>
+
+      <button id='gameAmountButton' onClick='setAmount(document.getElementById("gameAmountInput").value)'>Set Amount</button>
+ 
+      <script>
+      
+      var inventoryItems = ['Blue-Eyes White Dragon', 'A Dead Rat', 'Thunderfury', 'Hextech Gunblade'];
+      var inventoryAmount = ['3', '15', '1', '6'];
+      var inputvar = 0;
+
+      function inventorySearch(input)
+      {
+        inputvar = input;
+        document.getElementById('gameDisplay').innerHTML = inventoryItems[input] + " | Quantity: " + inventoryAmount[input];
+      }
+      
+      function setAmount(input2)
+      {
+        inventoryAmount[inputvar] = input2;
+        document.getElementById('gameDisplay').innerHTML = inventoryItems[inputvar] + " | Quantity: " + inventoryAmount[inputvar];
+      }
+      </script>
+      
 
     <!-- Place Answer Here -->
   </div>
@@ -37,8 +86,6 @@
         (Notice month is excluded)</h4>
     <!-- Place Answer Here -->
       
-      
-
     <!-- Place Answer Here -->
   </div>
 <!-- Question F -->
